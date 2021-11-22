583fe31e750876b511ac3e6c297da352d6af535b
Hill-Person <barkerty@gmail.com>
2021-11-22 15:10:01 -0500

    js work for order window

---
+++ 6-Top-Application/frontend/js/orderWindow.js
@@ -0,0 +1,22 @@
+const mainFloorPage = document.querySelector(".mainFloorPage");
+
+
+function openOrderWindow() {
+
+    const orderDiv = document.createElement("div")
+    orderDiv.className = "orderDiv";
+    mainFloorPage.appendChild(orderDiv);
+
+    const orderWindowCard = document.createElement("div")
+    orderWindowCard.className = "orderWindow";
+    orderDiv.appendChild(orderWindowCard);
+
+    const orderWindowHeaderText = document.createElement("h1")
+    orderWindowCard.innerText = "Needs fleshed out in here";
+    
+
+
+
+}
+
+export {openOrderWindow}
