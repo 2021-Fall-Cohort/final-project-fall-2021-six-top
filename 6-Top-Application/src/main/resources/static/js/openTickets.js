d1f3b9ebb3bd4b61714a7d34a423915860da492e
jaredphillips81 <90348394+jaredphillips81@users.noreply.github.com>
2021-12-03 11:25:59 -0500

    moved backend to root

---
+++ 6-Top-Application/src/main/resources/static/js/openTickets.js
@@ -0,0 +1,36 @@
+const mainFloorPage = document.querySelector(".orderDiv")
+
+function openTabsWindow() {
+
+    const openTabsDiv = document.createElement("div")
+    openTabsDiv.className = "openTabs";
+    mainFloorPage.appendChild(openTabsDiv);
+
+    const openTabsCard = document.createElement("div");
+    openTabsCard.className = "openTabs"
+    openTabsDiv.appendChild(openTabsCard);
+
+    const openTabsWindowText = document.createElement("h1");
+    openTabsCard.innerText = "Open Orders go here"
+
+    const foodItemsOrderedList = document.createElement("ul")
+    foodItemsOrderedList.className = "foodItemList";
+    openTabsCard.appendChild(foodItemsOrderedList);
+    // foodItemsOrderedList.innerText("Items ordered");
+
+    const foodListItem = document.createElement("li");
+    foodListItem.className = "foodListItem";
+    foodItemsOrderedList.appendChild(foodListItem);
+    foodListItem.innerText = "Burger";
+
+    const drinkItemsOrderedList = document.createElement("ul");
+    drinkItemsOrderedList.className = "drinkItemsOrderedList";
+    openTabsCard.appendChild(drinkItemsOrderedList);
+
+    const drinkListItem = document.createElement("li")
+    drinkListItem.className = "drinkListItem";
+    drinkItemsOrderedList.appendChild(drinkListItem);
+    drinkListItem.innerText = "stupid bud light";
+}
+
+export {openTabsWindow}
---
+++ 6-Top-Application/src/main/resources/static/js/orderWindow.js
@@ -0,0 +1,40 @@
+const mainFloorPage = document.querySelector(".mainFloorPage");
+
+// Open orders neeed to go below new ticket 
+// 
+
+function openOrderWindow() {
+
+    const orderDiv = document.createElement("div")
+    orderDiv.className = "orderDiv";
+    mainFloorPage.appendChild(orderDiv);
+
+    const orderWindowCard = document.createElement("div")
+    orderWindowCard.className = "orderWindow";
+    orderDiv.appendChild(orderWindowCard);
+
+    const orderWindowHeaderText = document.createElement("h1")
+    orderWindowCard.innerText = "New Order Window, needs orders";
+    // Generated ID in top left corner to specify order number
+
+    const kitchenButtonContainer = document.createElement("div")
+    kitchenButtonContainer.className = "kitchenButtonHolder"
+
+    const sendToKitchenButon = document.createElement("button");
+    sendToKitchenButon.className = "sendToKitchenButton";
+    sendToKitchenButon.innerText = "Send";
+    orderDiv.appendChild(kitchenButtonContainer);
+    kitchenButtonContainer.appendChild(sendToKitchenButon);
+
+    const splitTabButton = document.createElement("button");
+    splitTabButton.className = "splitTabButton";
+    splitTabButton.innerText = "Split Tab";
+    // split tab should go per order card per table
+
+
+    orderDiv.appendChild(kitchenButtonContainer);
+    kitchenButtonContainer.appendChild(splitTabButton);
+
+}
+
+export {openOrderWindow}
